6cafa5a902dd5ed7f11b83b8440ed7c532b4a1ce
Maki <mxmcube@gmail.com>
2022-08-22 20:21:16 -0400

    Add inter font

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,27 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "@fontsource/inter/100.css";
+import "@fontsource/inter/200.css";
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/inter/900.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+const theme = extendTheme({
+	fonts: {
+		heading: `"Inter", sans-serif`,
+		body: `"Inter", sans-serif`,
+	},
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<App />
 		</ChakraProvider>
 	</React.StrictMode>,
