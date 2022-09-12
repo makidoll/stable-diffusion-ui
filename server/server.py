5b30ea0be4f7fd6a17e0f80713b6b707527c9d4e
Maki <mxmcube@gmail.com>
2022-09-11 19:53:22 -0500

    Update generating to threading.Lock

--- server/server.py
+++ server/server.py
@@ -7,6 +7,7 @@ from io import BytesIO
 from pathlib import Path
 from time import sleep
 from urllib import request
+from threading import Lock
 
 from flask import (
     Flask, Response, jsonify, request, send_file, send_from_directory,
@@ -100,7 +101,7 @@ for result in db.table("generated").all():
 
 # web server api
 
-generating = False
+generating_lock = Lock()
 
 # step_previews = {}
 
@@ -120,225 +121,219 @@ generating = False
 
 @app.route("/api/generate", methods=["POST"])
 def generate():
-	global generating
-	if generating:
+	global generating_lock
+	if generating_lock.locked():
 		return jsonify({"error": "Busy generating another"}), 400
 
 	@stream_with_context
 	def generate_stream():
-		global generating
-		try:
-			generating = True
+		global generating_lock
+		with generating_lock:
+			try:
+				variations = 3
 
-			variations = 3
+				prompt = request.json["prompt"]
+				seed = int(request.json["seed"])
+				inference_steps = int(request.json["inferenceSteps"])
+				guidance_scale = float(request.json["guidanceScale"])
+				width = int(request.json["width"])
+				height = int(request.json["height"])
+				sampler = request.json["sampler"]
 
-			prompt = request.json["prompt"]
-			seed = int(request.json["seed"])
-			inference_steps = int(request.json["inferenceSteps"])
-			guidance_scale = float(request.json["guidanceScale"])
-			width = int(request.json["width"])
-			height = int(request.json["height"])
-			sampler = request.json["sampler"]
-
-			if inference_steps > 150:
-				generating = False
-				yield jsonify(
-				    {
-				        "error": "Please don't do more than 150 inference steps"
-				    }
-				).data
-				return
-
-			id = len(db.table("generated").all()) + 1
-
-			yield jsonify(
-			    {
-			        "id": id,
-			        "completed": 0,
-			        "variations": variations,
-			        "prompt": prompt,
-			        "percentage": 0
-			    }
-			).data
-
-			images = []
-			# step_previews[id] = {}
-
-			if make_test_images:
-				if seed == -1:
-					seed = random.randint(0, 2**32 - 1)
-
-				for i in range(0, variations):
-					sleep(1)
-
-					image = fakeImage(i, prompt)
-					filename = "id" + str(id) + "_v" + str(i) + ".png"
-					save_path = os.path.join(data_path, filename)
-					image.save(save_path)
-					images.append(image)
-
+				if inference_steps > 150:
 					yield jsonify(
 					    {
-					        "completed": i + 1,
-					        "percentage": ((i + 1) / variations) * 100
+					        "error":
+					            "Please don't do more than 150 inference steps"
 					    }
 					).data
-			else:
-				if seed == -1:
-					seed = generate_image.generate_seed()
+					return
 
-				for i in range(0, variations):
-					# step_previews[id][i] = {}
+				id = len(db.table("generated").all()) + 1
 
-					def yield_on_step(step):
-						# step_previews[id][i][step] = x
-						return jsonify(
+				yield jsonify(
+				    {
+				        "id": id,
+				        "completed": 0,
+				        "variations": variations,
+				        "prompt": prompt,
+				        "percentage": 0
+				    }
+				).data
+
+				images = []
+				# step_previews[id] = {}
+
+				if make_test_images:
+					if seed == -1:
+						seed = random.randint(0, 2**32 - 1)
+
+					for i in range(0, variations):
+						sleep(1)
+
+						image = fakeImage(i, prompt)
+						filename = "id" + str(id) + "_v" + str(i) + ".png"
+						save_path = os.path.join(data_path, filename)
+						image.save(save_path)
+						images.append(image)
+
+						yield jsonify(
 						    {
-						        # "step": step,
-						        "percentage":
-						            (
-						                (i / variations) +
-						                (step / inference_steps / variations)
-						            ) * 100
+						        "completed": i + 1,
+						        "percentage": ((i + 1) / variations) * 100
+						    }
+						).data
+				else:
+					if seed == -1:
+						seed = generate_image.generate_seed()
+
+					for i in range(0, variations):
+						# step_previews[id][i] = {}
+
+						def yield_on_step(step):
+							# step_previews[id][i][step] = x
+							return jsonify(
+							    {
+							        # "step": step,
+							        "percentage":
+							            (
+							                (i / variations) + (
+							                    step / inference_steps /
+							                    variations
+							                )
+							            ) * 100
+							    }
+							).data
+
+						result = yield from generate_image.generate_image(
+						    prompt=prompt,
+						    seed=seed + i,
+						    width=width,
+						    height=height,
+						    ddim_steps=inference_steps,
+						    cfg_scale=guidance_scale,
+						    yield_on_step=yield_on_step,
+						    check_safety=False,
+						    sampler=sampler
+						)
+
+						image = result["image"]
+						warning = result["prompt_length_warning"]
+						# has_nsfw_concept = result["has_nsfw_concept"]
+
+						filename = "id" + str(id) + "_v" + str(i) + ".png"
+						save_path = os.path.join(data_path, filename)
+						image.save(save_path)
+						images.append(image)
+
+						yield jsonify(
+						    {
+						        "completed": i + 1,
+						        "percentage": ((i + 1) / variations) * 100
 						    }
 						).data
 
-					result = yield from generate_image.generate_image(
-					    prompt=prompt,
-					    seed=seed + i,
-					    width=width,
-					    height=height,
-					    ddim_steps=inference_steps,
-					    cfg_scale=guidance_scale,
-					    yield_on_step=yield_on_step,
-					    check_safety=False,
-					    sampler=sampler
-					)
+				data = {
+				    "prompt": prompt,
+				    "seed": seed,
+				    "inferenceSteps": inference_steps,
+				    "guidanceScale": guidance_scale,
+				    "width": width,
+				    "height": height,
+				    "sampler": sampler,
+				    # other
+				    "variations": variations,
+				    "created": datetime.datetime.utcnow().isoformat() + "Z"
+				}
 
-					image = result["image"]
-					warning = result["prompt_length_warning"]
-					# has_nsfw_concept = result["has_nsfw_concept"]
+				make_preview(images, id, width, height, variations)
 
-					filename = "id" + str(id) + "_v" + str(i) + ".png"
-					save_path = os.path.join(data_path, filename)
-					image.save(save_path)
-					images.append(image)
+				# should be the same as id but this is more correct
+				id = db.table("generated").insert(data)
 
-					yield jsonify(
-					    {
-					        "completed": i + 1,
-					        "percentage": ((i + 1) / variations) * 100
-					    }
-					).data
+				# add to response
+				data["finished"] = True
+				data["warning"] = warning
+				data["id"] = id
 
-			data = {
-			    "prompt": prompt,
-			    "seed": seed,
-			    "inferenceSteps": inference_steps,
-			    "guidanceScale": guidance_scale,
-			    "width": width,
-			    "height": height,
-			    "sampler": sampler,
-			    # other
-			    "variations": variations,
-			    "created": datetime.datetime.utcnow().isoformat() + "Z"
-			}
+				yield jsonify(data).data
 
-			make_preview(images, id, width, height, variations)
-
-			# should be the same as id but this is more correct
-			id = db.table("generated").insert(data)
-
-			# add to response
-			data["finished"] = True
-			data["warning"] = warning
-			data["id"] = id
-
-			generating = False
-
-			yield jsonify(data).data
-
-		except Exception as e:
-			generating = False
-			print(repr(e))
-			yield jsonify(
-			    {
-			        "error": "Something went wrong, try again soon"
-			    }
-			).data
-			raise e
+			except Exception as e:
+				print(repr(e))
+				yield jsonify(
+				    {
+				        "error": "Something went wrong, try again soon"
+				    }
+				).data
+				raise e
 
 	return Response(generate_stream())
 
 @app.route("/api/generate/oneoff", methods=["POST"])
 def generate_oneoff():
-	global generating
-	if generating:
+	global generating_lock
+	if generating_lock.locked():
 		return jsonify({"error": "Busy generating another"}), 400
 
 	@stream_with_context
 	def generate_oneoff_stream():
-		global generating
-		generating = True
+		global generating_lock
+		with generating_lock:
+			variations = 3
 
-		variations = 3
+			try:
+				prompt = request.json["prompt"]
 
-		try:
-			prompt = request.json["prompt"]
+				images = []
+				unsafe = []
 
-			images = []
-			unsafe = []
+				if make_test_images:
 
-			if make_test_images:
+					for i in range(0, variations):
+						sleep(1)
 
-				for i in range(0, variations):
-					sleep(1)
+						images.append(fakeImage(i, prompt))
 
-					images.append(fakeImage(i, prompt))
+				else:
+					seed = generate_image.generate_seed()
 
-			else:
-				seed = generate_image.generate_seed()
+					for i in range(0, variations):
 
-				for i in range(0, variations):
+						result = yield from generate_image.generate_image(
+						    prompt=prompt,
+						    seed=seed + i,
+						    width=512,
+						    height=512,
+						    ddim_steps=50,
+						    cfg_scale=7.5,
+						    check_safety=True,
+						    sampler="k_lms"
+						)
 
-					result = yield from generate_image.generate_image(
-					    prompt=prompt,
-					    seed=seed + i,
-					    width=512,
-					    height=512,
-					    ddim_steps=50,
-					    cfg_scale=7.5,
-					    check_safety=True,
-					    sampler="k_lms"
+						images.append(result["image"])
+						unsafe.append(result["has_nsfw_concept"])
+
+				images_as_base64 = []
+
+				for image in images:
+					image_io = BytesIO()
+					image.save(image_io, "PNG")
+					image_io.seek(0)
+					images_as_base64.append(
+					    "data:image/png;base64," +
+					    base64.b64encode(image_io.read()).decode("ascii")
 					)
 
-					images.append(result["image"])
-					unsafe.append(result["has_nsfw_concept"])
+				data = {"images": images_as_base64, "unsafe": unsafe}
 
-			images_as_base64 = []
-
-			for image in images:
-				image_io = BytesIO()
-				image.save(image_io, "PNG")
-				image_io.seek(0)
-				images_as_base64.append(
-				    "data:image/png;base64," +
-				    base64.b64encode(image_io.read()).decode("ascii")
-				)
-
-			data = {"images": images_as_base64, "unsafe": unsafe}
-
-			generating = False
-
-			yield jsonify(data).data
-		except Exception as e:
-			generating = False
-			print(repr(e))
-			yield jsonify(
-			    {
-			        "error": "Something went wrong, try again soon"
-			    }
-			).data
+				yield jsonify(data).data
+			except Exception as e:
+				print(repr(e))
+				yield jsonify(
+				    {
+				        "error": "Something went wrong, try again soon"
+				    }
+				).data
 
 	return Response(generate_oneoff_stream())
 
